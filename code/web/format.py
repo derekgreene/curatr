422a8ad72657532dcd3bd7c641d008a43931dd91
Derek Greene <derek.greene@ucd.ie>
2023-01-12 16:56:26 +0000

    Add web formatting utilities

---
+++ code/web/format.py
@@ -0,0 +1,183 @@
+"""
+Generall formatting fuctions for display Curatr web content
+"""
+import urllib.parse
+# project imports
+from preprocessing.cleaning import tidy_extract
+
+# --------------------------------------------------------------
+
+field_name_map = {  "all" : "Full Text", "title_text" : "Title", "authors_text" : "Author", "location_text" : "Location" }
+field_plural_map = {  "title_text" : "titles", "authors_text" : "authors", "location_text" : "locations" }
+type_name_map = {  "volume" : "Volumes", "segment" : "Segments" }
+
+# --------------------------------------------------------------
+
+def format_classification_links(context):
+	""" Generate HTML formatting for links for book classifications on the classification index page. """
+	html = ""
+	class_counts = context.core.cache["class_counts"]
+	class_names = sorted(list(class_counts.keys()))
+	for name in class_names:
+		if name.lower() == "all" or class_counts[name] == 0:
+			continue
+		label = name.replace("?","'")
+		escaped_name = urllib.parse.quote_plus(name)
+		url = '%s/search?qwords=*&class="%s"&type=volume' % (context.prefix, escaped_name)
+		if class_counts[name] == 1:
+			html += "\t\t\t<li class='classification'><a href='%s'>%s</a> (1 book)\n" % (url, label)
+		else:
+			fmt_count = "{:,}".format(class_counts[name])
+			html += "\t\t\t<li class='classification'><a href='%s'>%s</a> (%s books)\n" % (url, label, fmt_count)
+	return html
+
+def format_subclassification_links(context):
+	""" Generate HTML formatting for links for book subclassifications on the classification index page. """
+	html = ""
+	subclass_counts = context.core.cache["top_subclass_counts"]
+	subclass_names = sorted(list(subclass_counts.keys()))
+	for name in subclass_names:
+		if name.lower() == "all" or subclass_counts[name] == 0:
+			continue
+		label = name
+		escaped_name = urllib.parse.quote_plus(name)
+		url = '%s/search?qwords=*&subclass="%s"&type=volume' % (context.prefix, escaped_name)
+		if subclass_counts[name] == 1:
+			html += "\t\t\t<li class='classification'><a href='%s'>%s</a> (1 book)\n" % (url, label)
+		else:
+			fmt_count = "{:,}".format(subclass_counts[name])
+			html += "\t\t\t<li class='classification'><a href='%s'>%s</a> (%s books)\n" % (url, label, fmt_count)
+	return html
+
+def format_classification_options(context, selected = "all"):
+	# add the all option
+	class_names = [ "all" ] + context.core.cache["class_names"]
+	# generate the HTML
+	html = ""
+	for name in class_names:
+		label = name.replace("?","'")
+		if name == "all":
+			label = "All Classifications"
+		# is this the currently selected option?
+		if name.lower() == selected.lower():
+			html += "<option value='%s' selected>%s</option>\n" % (name, label)
+		else:
+			html += "<option value='%s'>%s</option>\n" % (name, label)
+	return html
+
+def format_subclassification_options(context, selected = "all"):
+	# add the all option
+	subclass_names =  [ "all" ] + context.core.cache["subclass_names"]
+	# generate the HTML
+	html = ""
+	for name in subclass_names:
+		label = name.replace("?","'")
+		if name == "all":
+			label = "All Sub-classifications"
+		# is this the currently selected option?
+		if name.lower() == selected.lower():
+			html += "<option value='%s' selected>%s</option>\n" % (name, label)
+		else:
+			html += "<option value='%s'>%s</option>\n" % (name, label)
+	return html
+
+def format_location_options(context, selected = "all"):
+	location_names = context.core.cache["top_location_names"]
+	# add the all option
+	location_names = [ "all" ] + location_names
+	# generate the HTML
+	html = ""
+	for name in location_names:
+		label = name.replace("?","'")
+		if name == "all":
+			label = "All Locations"
+		# is this the currently selected option?
+		if name.lower() == selected.lower():
+			html += "<option value='%s' selected>%s</option>\n" % (name, label)
+		else:
+			html += "<option value='%s'>%s</option>\n" % (name, label)
+	return html
+
+def format_field_options(selected = "all"):
+	html = ""
+	field_keys = list(field_name_map.keys())
+	field_keys.sort()
+	for key in field_keys:
+		# is this the currently selected option?
+		if key == selected:
+			html += "<option value='%s' selected>%s</option>\n" % (key, field_name_map[key])
+		else:
+			html += "<option value='%s'>%s</option>\n" % (key, field_name_map[key])
+	return html	
+
+def format_type_options(selected = "volume"):
+	html = ""
+	type_keys = list(type_name_map.keys())
+	type_keys.sort()
+	for key in type_keys:
+		# is this the currently selected option?
+		if key == selected:
+			html += "<option value='%s' selected>%s</option>\n" % (key, type_name_map[key])
+		else:
+			html += "<option value='%s'>%s</option>\n" % (key, type_name_map[key])
+	return html	
+
+def format_mudies_options(selected = False):
+	html = ""
+	if selected:
+		html += "<option value='true' selected>Yes</option>\n" 
+		html += "<option value='false'>No</option>\n" 
+	else:
+		html += "<option value='true'>Yes</option>\n" 
+		html += "<option value='false' selected>No</option>\n" 
+	return html	
+	
+def format_volume_list(context, db, selected_volumes, verbose = True, max_title_length = 200):
+	html = ""
+	result_url_prefix = "%s/volume" % (context.prefix)
+	# Generate HTML	for each result
+	for volume_id in selected_volumes:
+		doc = db.get_volume_metadata(volume_id)
+		if doc is None:
+			continue
+		title = doc["title"]
+		if len(title) > max_title_length:
+			title = title[0:max_title_length] + "&hellip;"
+		year = int(doc["year"])
+		volume = int(doc["volume"])
+		max_volume = int(doc["volumes"])
+		url_result = "%s?id=%s" % (result_url_prefix, volume_id)
+		html += "<p class='search-result'>\n"
+		html += "<div class='search-result-meta'>\n"
+		# add extra metadata?
+		if verbose:
+			html += "<a href='%s' class='result-title'>%s" % (url_result, title)
+			if max_volume > 1:
+				html += "&nbsp;&nbsp;&ndash;&nbsp;&nbsp;Volume %d" % (volume)
+			html += "</a>\n"
+			if "authors" in doc:
+				sauthors = doc["authors"]
+			else:
+				sauthors = "Unknown author"
+			if "location" in doc:
+				html += "<div>%s &ndash; %s &ndash; %s</div>\n" % (sauthors, year, doc["location"])
+			else:
+				html += "<div>%s &ndash; %s</div>\n" % (sauthors, year)
+		# just add the primary metadata
+		else:
+			html += "<a href='%s' class='result-title'>%s&nbsp;&nbsp;(%d)&nbsp;&nbsp;&ndash;&nbsp;&nbspVolume %d</a>\n" % (url_result, title, year, volume)
+		html += "</div>\n"
+		# add a snippet to the result?
+		if verbose:
+			extract = db.get_volume_extract(volume_id)		
+			if extract is None or len(extract) == 0:
+				extract = "No text available."
+			else:
+				extract = tidy_extract(extract)
+			# actually format the snippet
+			html += "<ul class='search-snippets'>\n"
+			html += "<li class='snippet-text'>%s</li>\n" % extract
+			html += "</ul>\n"
+		# finished result
+		html += "</p>\n"
+	return html
---
+++ code/web/util.py
@@ -0,0 +1,76 @@
+"""
+Various utility functions used as part of the Curatr web search implementation.
+"""
+import re
+import logging as log
+
+def safe_int(value, default=0):
+	""" Make sure the specified value is a valid integer """
+	if value is None:
+		return default
+	if type(value) == int:
+		return value
+	svalue = str(value)
+	if len(svalue) == 0:
+		return default
+	try:
+		return int(svalue)
+	except:
+		return default
+
+def parse_arg_int(request, param_name, default=0):
+	svalue = request.args.get(param_name, default="").strip().lower()
+	if len(svalue) == 0:
+		return default
+	try:
+		return int(svalue)
+	except:
+		return default
+
+def parse_arg_bool(request, param_name, default=False):
+	svalue = request.args.get(param_name, default="").strip().lower()
+	return svalue == "1" or svalue == "true"
+
+def format_year_range(ystart, yend):
+	""" Nicely format a pair of years as a range string """
+	# make sure start year is valid
+	if ystart is None or ystart == "*":
+		ystart = 0
+	elif type(ystart) == str:
+		try:
+			ystart = int(ystart)
+		except:
+			ystart = 0
+	# make sure end year is valid
+	if yend is None or yend == "*":
+		yend = 0
+	elif type(yend) == str:
+		try:
+			yend = int(yend)
+		except:
+			yend = 0
+	# now build the format string
+	s_year = ""
+	if ystart > 0:
+		s_year = "From %d" % ystart
+	if yend > 0 and yend < 2000:
+		if len(s_year) == 0:
+			s_year = "Until %d" % yend
+		else:
+			s_year += " until %d" % yend
+	elif len(s_year) > 0:
+		s_year += " onwards" 
+	return s_year
+
+def parse_keyword_query(raw_query_string):
+	""" Query parsing where the user inputs a list of comma-separated keywords """
+	if raw_query_string == "*":
+		return ""
+	raw_query_string = re.sub("\s+", " ", raw_query_string).strip().lower()
+	queries = []
+	for query in raw_query_string.split(","):
+		query = re.sub("[^a-zA-Z0-9 ]", "", query).strip()
+		if len(query) > 1 and not query in queries:
+			queries.append(query)
+	return queries
+
