4d17b337f7469544eec258724d1bfb0491961dbf
Derek Greene <derek.greene@ucd.ie>
2026-03-16 19:08:57 +0000

    Minor fixes to admin page

--- code/web/admin.py
+++ code/web/admin.py
@@ -3,12 +3,12 @@ Implementation for corpus system administration-related features of the Curatr w
 """
 
 import operator
-from markupsafe import Markup
+from markupsafe import Markup, escape
 
 # --------------------------------------------------------------
 
 def format_user_list(context, db):
-	""" Produce a HTML formatted table continue details of users registered on the system """
+	""" Produce a HTML formatted table containing details of users registered on the system """
 	users = db.get_all_users()
 	users.sort(key=operator.attrgetter('last_login'))
 	users.reverse()
@@ -23,7 +23,7 @@ def format_user_list(context, db):
 			suffix += " (Admin)"
 		if user.guest:
 			suffix += " (Guest)"
-		html += "\t\t<td class='text-left user'>%s%s</td>" % (user.email, suffix)
+		html += "\t\t<td class='text-left user'>%s%s</td>" % (escape(user.email), suffix)
 		# user date created column
 		html += "\t\t<td class='text-left user'>%s</td>" % user.created_at.strftime('%Y-%m-%d %H:%M')
 		# user last login column
@@ -35,6 +35,6 @@ def format_user_list(context, db):
 		html += "\t\t<td class='text-left user'>%s</td>" % user.num_logins
 		# add actions
 		url_edit = "%s/useredit?user_id=%d" % (context.prefix, user.id)
-		html += "<td class='text-center lex'><a href='%s'><img src='%s/img/edit.png' width='30px' style=''/></a></td>\n" % (url_edit, context.staticprefix)
+		html += "\t\t<td class='text-center lex'><a href='%s'><img src='%s/img/edit.png' width='30px'></a></td>\n" % (url_edit, context.staticprefix)
 		html += "</tr>\n"
 	return html
